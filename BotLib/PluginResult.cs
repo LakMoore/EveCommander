d3eb7f06b13a75dc630dd029c2606a148e56009c
Lak Moore <LakMoore@after-ice.com>
2026-06-28 22:43:44 +0100

    Better convenience methods for PluginResult

--- BotLib/PluginResult.cs
+++ BotLib/PluginResult.cs
@@ -4,11 +4,21 @@ namespace BotLib
 {
   public class PluginResult
   {
-    public static PluginResult True 
-      => new() { WorkDone = true, Message = null, Background = null, Foreground = null };
+    public static PluginResult True(string? message = null)
+      => new() { WorkDone = true, Message = message, Background = null, Foreground = null };
 
-    public static PluginResult False
-      => new() { WorkDone = false, Message = null, Background = null, Foreground = null };
+    public static PluginResult False(string? message = null)
+      => new() { WorkDone = false, Message = message, Background = null, Foreground = null };
+
+    public static PluginResult Success(string message)
+    {
+      return new() { WorkDone = false, Message = message, Background = Color.Green, Foreground = Color.White };
+    }
+
+    public static PluginResult Failure(string message)
+    {
+      return new() { WorkDone = false, Message = message, Background = Color.Red, Foreground = Color.White };
+    }
 
     public required bool WorkDone;
     public string? Message;
@@ -16,15 +26,11 @@ namespace BotLib
     public Color? Foreground;
 
     public static implicit operator PluginResult(bool value)
-      => value ? True : False;
+      => value ? True() : False();
 
     // can cast to bool
     public static implicit operator bool(PluginResult result)
       => result.WorkDone;
 
-    public static PluginResult Failure(string message)
-    {
-      return new() { WorkDone = false, Message = message, Background = Color.Red, Foreground = Color.White };
-    }
   }
 }
